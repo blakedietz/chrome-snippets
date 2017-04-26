f953a988da00cb4d8c570aa770ec14160de3c4c7
Blake Dietz <blakedietz@gmail.com>
2017-04-26 15:33:21 -0600

    Add some documentation.

--- json-utilities.js
+++ json-utilities.js
@@ -1,3 +1,7 @@
+/**
+ * A generic template generator that will generate a string given an object to serialize and a 
+ * formatter function that accepts a key as an argument
+ */
 const convertJsonToTemplateString = (formatterFunction) => (objectToSerialize) => {
    return Object.keys(objectToSerialize).reduce((finalString, key) => {
     return finalString + formatterFunction(key);
@@ -5,11 +9,34 @@ const convertJsonToTemplateString = (formatterFunction) => (objectToSerialize) =
   , '');
 };
 
+/**
+ * Converts a json object to key value pairs for a constructor function of a model that represents 
+ *  the json.
+ * E.g. { foo: 'bar' }
+ * The out put of this function bould be the body of the constructor function below.
+ *
+ * class Foo {
+ *   constructor(config) {
+ *      this.foo = config.bar;
+ *   }
+ * }
+ */
 function convertJsonToObjectConstructorString(object) {
   const formatter = key => `this.${key} = config.${key}; \n`;
   return convertJsonToTemplateString(formatter)(object);
 }
 
+/**
+ * Converts a javascript object that is a map of parameters to a function to JSDoc format.
+ * For example:
+ * const someParams = { foo: 'bar', baz: 'boo' };
+ * someFunc(someParams);
+ *
+ * The jsdoc string would be generated as follows:
+ *
+ * @param someParams.foo
+ * @param someParams.baz
+ */
 function convertJsonToParameterObjectDocumentationString(parameterName, object) {
   const formatter = key => `@param ${parameterName}.${key} \n`;
   return convertJsonToTemplateString(formatter)(object);
